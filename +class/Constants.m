8104ebde35533ad12dfbec33a442b006b844aaa9
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-08-05 13:59:14 -0400

    Revision before compilation (2/2)

--- +class/Constants.m
+++ +class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'SCH'
         appRelease    = 'R2024a'
-        appVersion    = '0.81'
+        appVersion    = '0.82'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 880, 660]
--- +suggestion/getCacheColumnName.m
+++
@@ -1,8 +0,0 @@
-function cacheColumnName = getCacheColumnName(tableVariableNames, columnName)
-
-    cacheColumnName = sprintf('_%s', columnName);
-    if ~ismember(cacheColumnName, tableVariableNames)
-        cacheColumnName = columnName;
-    end
-
-end
