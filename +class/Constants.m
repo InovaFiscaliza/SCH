a40e0f82e789ed81ba030c1880c3259711925188
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-08-09 12:19:03 -0400

    Últimos ajustes no módulo FiscalizaLib

--- +class/Constants.m
+++ +class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'SCH'
         appRelease    = 'R2024a'
-        appVersion    = '0.85'
+        appVersion    = '0.87'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 880, 660]
