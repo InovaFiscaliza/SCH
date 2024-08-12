851aea1beb3d79dfeb41123d45612658cba1838c
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-08-12 08:57:04 -0400

    Update winSCH.mlapp
    macOS Compatibility

--- +class/Constants.m
+++ +class/Constants.m
@@ -4,7 +4,7 @@ classdef (Abstract) Constants
         %-----------------------------------------------------------------%
         appName       = 'SCH'
         appRelease    = 'R2024a'
-        appVersion    = '0.87'
+        appVersion    = '0.88'
 
         windowSize    = [1244, 660]
         windowMinSize = [ 880, 660]
